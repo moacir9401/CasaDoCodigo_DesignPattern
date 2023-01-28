f539f5aed23d039b3dee925c750700f2129482ae
moacir <moacir9401@gmail.com>
2023-01-28 17:36:08 -0300

    implementando singleton em um jogo de futebol para não instanciar mais de uma bola

---
+++ Singleton/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using AbstractFactory.Models;
+
+namespace CasadoCodigo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Singleton jogador_1 = Singleton.GetInstalcia;
+
+            jogador_1.Mensagem("Jogadir 1: A bola está comigo no meio do campo");
+
+            Singleton jogador_2 = Singleton.GetInstalcia;
+
+            jogador_2.Mensagem("Jogadir 2: recebeu a bola");
+
+            Singleton jogador_3 = Singleton.GetInstalcia;
+
+            jogador_3.Mensagem("Jogadir 3: recebeu  o lançamento na linha de fundo");
+             
+            Console.ReadKey();
+        }
+
+
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,26 @@
+﻿namespace AbstractFactory.Models
+{
+    public sealed class Singleton
+    {
+        public static Singleton instancia = null;
+
+        public static Singleton GetInstalcia
+        {
+            get
+            {
+                if (instancia == null)
+                {
+                    instancia = new Singleton();
+                    Console.WriteLine("Bola em jogo");
+                }
+
+                return instancia;
+            }
+        }
+
+        public void Mensagem(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
