c16fe564e922826c8040595385e8e36a065a1bcc
Moacir Afonso <moaciralves0194@gmail.com>
2023-04-29 21:42:31 -0300

    criando modelo de fases do jogo utilizando template method

---
+++ TemplateMethod/Models/Jogo.cs
@@ -0,0 +1,19 @@
+﻿namespace TemplateMethod.Models
+{
+    public abstract class Jogo
+    {
+        public Jogo()
+        {
+            TrilhaSonora();
+            PrimeiraFase();
+            SegundaFase();
+        }
+        public abstract void PrimeiraFase();
+        public abstract void SegundaFase();
+        private void TrilhaSonora()
+        {
+            Console.WriteLine("Música emocionante");
+        }
+    }
+
+}
---
+++ TemplateMethod/Models/ModoNormal.cs
@@ -0,0 +1,16 @@
+﻿namespace TemplateMethod.Models
+{
+    public class ModoNormal : Jogo
+    {
+        public override void PrimeiraFase()
+        {
+            Console.WriteLine("O carro precisa abastecer uma vez");
+        }
+        public override void SegundaFase()
+        {
+            Console.WriteLine("Os carros devem correr na mesma ve locidade");             
+        }
+    }
+
+
+}
